d63002cd898ba22e940b28363e61946141bbcfff
AmauriMMontejo <amauri.montejoo@gmail.com>
2026-06-30 17:10:00 -0600

    Refactor Call classes with comments and explanations
    Updated CallOrigin, CustomerCall, and ProviderCall classes with additional comments and explanations. Enhanced the CallBuilder class with comments regarding the builder pattern implementation.

--- Proyecto_2/Calls.ts
+++ Proyecto_2/Calls.ts
@@ -3,7 +3,7 @@ export abstract class CallOrigin {
     abstract disposition(): string[];
 }
 
-//Llamada de cliente
+//Llamada de cliente en esta parte se hace el uso de el patron abstract factory porque tenemos la parte de 2 tipos de llamada que implemetan el CallOrigin... En un futuro lo pienso ampliar
 export class CustomerCall extends CallOrigin{
     type = 'Client';
     disposition(): string[] {//Sus dispositions que ocupan cada uno
@@ -32,6 +32,7 @@ export class CallForm {
     public disposition!: string;
 }
 
+//El call builder es el patron builder fluent que ocupamos para hacer la parte de ir creando la llamda que tenemos poco a poco
 export class CallBuilder {
     private form = new CallForm();
     private originO!: CallOrigin;
@@ -43,6 +44,7 @@ export class CallBuilder {
     }
 
     public setrandomPhone(): this {
+        //se hace una formula de la parte de el numero para que sea random y pueda quedar completo el numero
         this.form.phone = `55-${Math.floor(100 + Math.random() * 9000)}-${Math.floor(1000 + Math.random() * 9000)}`;
         return this;
     }
@@ -50,6 +52,7 @@ export class CallBuilder {
     public setBilling(code: string): this {
         const billing = code.trim();
         if (billing.length !== 4) {
+            //se verifica que sea correcto el billing con que sea 4 y en minuscula
             throw new Error('Billing debe tener exactamente 4 caracteres.');
         }
         this.form.billing = billing.toLowerCase();
@@ -60,6 +63,7 @@ export class CallBuilder {
         const disposition = disp.trim().toLowerCase();
         const options = this.originO?.disposition() ?? [];
         if (!options.includes(disposition)) {
+            //Se hace la validacion del disposition en donde en efetco sea algo que si este guardado en la lista de disposition si no ni entra
             throw new Error(`Disposition inválido para origen ${this.originO?.type ?? 'desconocido'}: ${disp}`);
         }
         this.form.disposition = disposition;
